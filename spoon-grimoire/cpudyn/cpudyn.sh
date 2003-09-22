ec20de54b9b243a2d81a855044122ab1ff8cfc72
jarred <jarred>
2003-09-22 15:29:53 +0000

    Initial revision

---
+++ spoon-grimoire/cpudyn/cpudyn.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ARGS="-d -i 1 -p 0.5 0.90"
+#ARGS="$ARGS -t 120 -h /dev/hda"
+
+start() {
+  echo -n "Starting cpudynd: "
+  cpudynd $ARGS
+  if [ $? = 0 ]; then
+    echo "cpudynd."
+  else
+    echo "(failed.)"
+  fi
+}
+
+stop() {
+  echo -n "Stopping cpudynd: "
+  pkill cpudynd
+  echo "cpudynd."
+}
+
+case "$1" in
+  start)   start ;;
+  stop)    stop  ;;
+  restart) $0 stop
+           $0 start
+           ;;
+  *)       echo "Usage: $0 {start|stop|restart}"
+           exit 1
+           ;;
+esac
